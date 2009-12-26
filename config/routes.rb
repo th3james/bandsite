66916e4305da98097b320dcfca74a027021f1008
James Cox <th3james@gmail.com>
2009-12-26 20:55:31 +0000

    CSS added, root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller=>:posts, :action=>:index
+  
   map.resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
