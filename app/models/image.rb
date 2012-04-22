897227a55fd7383756fe9f0c972d0e1b88e25b31
James Cox <th3james@gmail.com>
2012-04-22 18:56:59 +0100

    Fix image sizing

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,3 @@
 class Image < ActiveRecord::Base
-  has_attached_file :image, :styles => { :small => "600x600>", :small => "275x275#", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :normal => "600x600>", :small => "275x275#", :thumb => "100x100>" }
 end
